a88f0b738129cb5bd4494bad40a3df82e247874d
ThermalSpan <ThermalSpan@protonmail.com>
2018-06-05 00:12:59 -0700

    Added comments to march_cube

--- src/marching_cubes_impl.rs
+++ src/marching_cubes_impl.rs
@@ -26,6 +26,11 @@ pub fn march_cube<E>(values: &[f32; 8], mut edge_func: E)
 where
     E: FnMut(usize) -> (),
 {
+    // We need to construct an index into the TRIANGLE_CONNECTION table.
+    // This is the table of possible triangulation topologies that is the
+    // signature of marching cubes. Note that value of our source at each vertex
+    // is <= 0 or it isn't. So there are 2^8 = 256 possible combinations of vertices
+    // that are <= 0. Therefore are 256 entries in the triangle connection table
     let mut cube_index = 0;
     for i in 0..8 {
         if values[i] <= 0.0 {
@@ -33,11 +38,27 @@ where
         }
     }
 
+    // i is the "triangle index", where each cube's triangulation can include
+    // up to 4 triangles
     for i in 0..5 {
+        // Each entry in the TRIANGLE_CONNECTION table is a [i8; 16].
+        // There can be at most 4 triangles, with 3 vertices each, which means we need at least
+        // 12 bytes, but rounding up to 16 makes for good aligment in memory. Unused values are
+        // set to -1.
+        //
+        // So the first thing to do is make sure that we have an i triangle at all
         if TRIANGLE_CONNECTION[cube_index][3 * i] < 0 {
             break;
         }
 
+        // Now we need to process each edge in the triangle. This means interpolating the vertices
+        // of the edge, to get the triangle's vertex. However, most vertices are shared among
+        // several triangles.
+        //
+        // This is where the use of the edge_func comes in:
+        // We could simply dump the index / vertex into a buffer (since we can count by three
+        // in each and get the triangles back). Or we can do some caching by a unique edge index
+        // to be smarter about about sharing vertices
         for j in 0..3 {
             let edge = TRIANGLE_CONNECTION[cube_index][3 * i + j] as usize;
 
